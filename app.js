1bcacb24996c53b1e237ce404b220ea4f3e5ec81
Mohamed Safras <awsafras@gmail.com>
2022-09-15 12:04:00 +0530

    sign in and sign up page added

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const nav_links = document.querySelector("#nav_links");
 const nav_link = nav_links.querySelectorAll(".nav_link");
-nav_links.addEventListener("click", (e) => {
-  const target = e.target.closest(".nav_link");
+const nav_bar = document.querySelector(".nav_bar");
 
-  if (!target) return;
+class App {
+  constructor() {
+    nav_links.addEventListener("click", this._changeTab);
+    window.addEventListener("scroll", this._toggleNavBar);
+  }
 
-  nav_link.forEach((item) => {
-    item.classList.remove("active");
-  });
+  _changeTab(e) {
+    const target = e.target.closest(".nav_link");
 
-  target.classList.add("active");
-});
+    if (!target) return;
+
+    nav_link.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    target.classList.add("active");
+  }
+  _toggleNavBar() {
+    if (window.pageYOffset > nav_bar.offsetHeight) {
+      nav_bar.classList.add("sticky");
+    } else {
+      nav_bar.classList.remove("sticky");
+    }
+  }
+}
+
+const app = new App();
