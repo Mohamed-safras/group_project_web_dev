a73c49007e686f7d73f7a65bf62fcf8f6a41d6e4
Mohamed Safras <awsafras@gmail.com>
2022-09-19 14:50:47 +0530

    register page has been finished

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const nav_links = document.querySelector("#nav_links");
-const nav_link = nav_links.querySelectorAll(".nav_link");
-const nav_bar = document.querySelector(".nav_bar");
+// const nav_links = document.querySelector("#nav_links");
+// const nav_link = nav_links.querySelectorAll(".nav_link");
+// const nav_bar = document.querySelector(".nav_bar");
 
-class App {
-  constructor() {
-    nav_links.addEventListener("click", this._changeTab);
-    window.addEventListener("scroll", this._toggleNavBar);
-  }
+// class App {
+//   constructor() {
+//     nav_links.addEventListener("click", this._changeTab);
+//     window.addEventListener("scroll", this._toggleNavBar);
+//   }
 
-  _changeTab(e) {
-    const target = e.target.closest(".nav_link");
+//   _changeTab(e) {
+//     const target = e.target.closest(".nav_link");
 
-    if (!target) return;
+//     if (!target) return;
 
-    nav_link.forEach((item) => {
-      item.classList.remove("active");
-    });
+//     nav_link.forEach((item) => {
+//       item.classList.remove("active");
+//     });
 
-    target.classList.add("active");
-  }
-  _toggleNavBar() {
-    if (window.pageYOffset > nav_bar.offsetHeight) {
-      nav_bar.classList.add("sticky");
-    } else {
-      nav_bar.classList.remove("sticky");
-    }
-  }
-}
+//     target.classList.add("active");
+//   }
+//   _toggleNavBar() {
+//     if (window.pageYOffset > nav_bar.offsetHeight) {
+//       nav_bar.classList.add("sticky");
+//     } else {
+//       nav_bar.classList.remove("sticky");
+//     }
+//   }
+// }
 
-const app = new App();
+// const app = new App();
